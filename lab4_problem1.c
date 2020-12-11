b87885073410e9b701b58fa51ac432497ac2e20a
Wayland Chang <32940536+wchan051@users.noreply.github.com>
2020-12-11 15:26:31 -0800

    Update lab4_problem1.c

--- lab4_problem1.c
+++ lab4_problem1.c
@@ -8,20 +8,20 @@ int main ()
     int i;
     double x = 0.0;
     double pi = 0.0;
-    double sum = 0.0;
+    double total_sum = 0.0;
     double t;
     step = 1.0/(double)num_steps;
     t = omp_get_wtime();
-    #pragma omp parallel private(i,x) reduction(+:sum)
+    #pragma omp parallel private(i,x) reduction(+:total_sum)
     {
         int id = omp_get_thread_num();
         int nth = omp_get_num_threads();
         for(i = id; i < num_steps; i += nth) {
             x = (i + 0.5) * step;
-            sum = sum + 4.0 / (1.0 + x*x);
+            total_sum = total_sum + 4.0 / (1.0 + x*x);
         }
     }
     t = omp_get_wtime() - t;
-    pi = step * sum;
+    pi = step * total_sum;
     printf("pi=%f,time=%f\n",pi,t);
 }
