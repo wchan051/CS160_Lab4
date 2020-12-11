0fcc470b32c5448d729638241109f6af91bda3f9
Wayland Chang <32940536+wchan051@users.noreply.github.com>
2020-12-11 14:51:19 -0800

    Update lab4_problem1.c

--- lab4_problem1.c
+++ lab4_problem1.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include "omp.h"
 #define MAX_THREADS 4
-static long num_steps = 100000;
+static long num_steps = 1000000;
 double step;
 int main ()
 {
     int i;
-    double pi, full_sum = 0.0;
-    double start_time, run_time, x;
-    step = 1.0 / (double) num_steps;
-    
-    omp_set_num_threads(MAX_THREADS);
-    full_sum = 0.0;
-    start_time = omp_get_wtime();
-    #pragma omp parallel private(x), private(i)
+    double x = 0.0;
+    double pi = 0.0;
+    double sum = 0.0;
+    double t;
+    step = 1.0/(double)num_steps;
+    t = omp_get_wtime();
+    #pragma omp parallel private(i,x) reduction(+:sum)
     {
         int id = omp_get_thread_num();
-        int numthreads = omp_get_num_threads();
-        double partial_sum = 0;
-        for (i = id; i < num_steps; i += numthreads){
-            x = (i+0.5) * step;
-            partial_sum = partial_sum + 4.0/(1.0+x*x);
+        int nth = omp_get_num_threads();
+        for(i = id; i < num_steps; i += nth) {
+            x = (i + 0.5) * step;
+            sum = sum + 4.0 / (1.0 + x*x);
         }
-        full_sum += partial_sum;
     }
-    pi = step * full_sum;
-    run_time = omp_get_wtime() - start_time;
-    printf("\npi is %f in %f seconds %d threads \n", pi, run_time, MAX_THREADS);
+    t = omp_get_wtime() - t;
+    pi = step * sum;
+    printf("pi=%f,time=%f\n",pi,t);
 }
